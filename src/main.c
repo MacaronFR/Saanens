15b3c78c0a47809a7c15519768d3a288a03965a5
MacaronFR <denisft77@gmail.com>
2020-12-12 10:28:34 +0100

    Ajout d'un style pour l'appli

--- src/main.c
+++ src/main.c
@@ -6,6 +6,9 @@ G_MODULE_EXPORT char *send_command(GtkWidget *textView, GdkEvent *event, gpointe
 int main(int argc,char **argv){
 	GtkBuilder *builder;
 	GtkWidget *window;
+	GtkCssProvider *provider;
+	GdkDisplay *display;
+	GdkScreen *screen;
 	gtk_init(&argc,&argv);
 	builder = gtk_builder_new();
 	gtk_builder_add_from_file(builder, "../test.glade",NULL);
@@ -16,6 +19,13 @@ int main(int argc,char **argv){
 	}
 	gtk_builder_connect_signals(builder, window);
 	g_object_unref(builder);
+
+	provider = gtk_css_provider_new();
+	display = gdk_display_get_default();
+	screen = gdk_display_get_default_screen(display);
+	gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+	gtk_css_provider_load_from_path(GTK_CSS_PROVIDER(provider),"../src/style.css", NULL);
+
 	gtk_widget_show(window);
 	gtk_main();
 	return 0;
@@ -29,7 +39,7 @@ G_MODULE_EXPORT char *send_command(GtkWidget *textView, GdkEvent *event, gpointe
 	if(event->key.keyval == GDK_KEY_Return) {
 		gchar *contents, *locale;
 		GtkTextIter start, end;
-		GtkTextBuffer *buffer = gtk_text_view_get_buffer((GtkTextView *)textView);
+		GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
 		gtk_text_buffer_get_bounds(buffer, &start, &end);
 		contents = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
 		locale = g_locale_from_utf8(contents, -1, NULL, NULL, NULL);
