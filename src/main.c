47b6f24dec08b3d8ab894dd2c84cd48fb4da91d9
MacaronFR <denisft77@gmail.com>
2020-12-21 11:38:17 +0100

    interpréteur propre en CLI

--- src/main.c
+++ src/main.c
@@ -4,30 +4,13 @@
 extern s_vars vartab;
 
 int main(int argc,char **argv){
-	s_var temp;
-	vartab.length = 2;
-	vartab.tab = malloc(sizeof(s_var) * 2);
-	temp.name = "TEST";
-	temp.type = 1;
-	temp.value.undefined = True;
-	vartab.tab[0] = temp;
-	temp.name = "TEST2";
-	temp.type = 2;
-	temp.value.undefined = True;
-	temp.value.ve = 10;
-	printf("%d",temp.value.undefined);
-	vartab.tab[1] = temp;
-	int test1 = 7;
-	printf("***%d***\n",vartab.length);
-	if(!new_var(&test1, "TEST3", 5, S_ENT)){
-		perror("Error on new value");
-	}
-	interpret_command("int a = 5;");
-	printf("\n**%d**\n", vartab.length);
-	char * type;
-	for(int i = 0; i < vartab.length; ++i){
-		type = get_name_from_type(vartab.tab[i].type);
-		printf("Name = %s, Type = %s\n", vartab.tab[i].name, type);
+	char input[1024];
+	printf("Saanens V0.1\n");
+	while(strcmp(input,"exit") != 0){
+		printf("> ");
+		scanf("%s",input);
+		fflush(stdin);
+		interpret_command(input);
 	}
 	return 0;
 }
